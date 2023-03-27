d4268595779f1016ee8bc450fb5881083df1f0d4
Алексей Кирсанов <alexbalderlab@gmail.com>
2023-03-27 21:45:20 +0300

    add brain-even.js

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/AlexMaster001/frontend-project-44/workflows/hexlet-check/badge.svg)](https://github.com/AlexMaster001/frontend-project-44/actions)
+<a href="https://codeclimate.com/github/AlexMaster001/frontend-project-44/maintainability"><img src="https://api.codeclimate.com/v1/badges/e845f97be52e42f4aa2f/maintainability" /></a>
---
+++ src/games/even.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import random from '../random.js';
+
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+const even = () => {
+  const numb = random(1, 100);
+  console.log(`Question: ${numb}`);
+  const answer = numb % 2 === 0 ? 'yes' : 'no';
+  const userAnswer = readlineSync.question('Your answer: ');
+  return [answer, userAnswer];
+};
+
+export default even;
