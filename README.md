6ba3b4400ee43672ee88b4fa2e75a634298721f3
Алексей Кирсанов <alexbalderlab@gmail.com>
2023-03-30 14:16:40 +0300

    add calc

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/AlexMaster001/frontend-project-44/workflows/hexlet-check/badge.svg)](https://github.com/AlexMaster001/frontend-project-44/actions)
-<<<<<<< HEAD
 ### Maintainability status:
 <a href="https://codeclimate.com/github/AlexMaster001/frontend-project-44/maintainability"><img src="https://api.codeclimate.com/v1/badges/e845f97be52e42f4aa2f/maintainability" /></a>
 ### brain-even test:
 https://asciinema.org/a/OkhRuT1kggriICnHoWTJn6tks
-=======
-<a href="https://codeclimate.com/github/AlexMaster001/frontend-project-44/maintainability"><img src="https://api.codeclimate.com/v1/badges/e845f97be52e42f4aa2f/maintainability" /></a>
->>>>>>> 11d240b8925b357c55a49d1e5f0a19388e308517
+https://asciinema.org/a/cptz6b1an3oM7jvkjkvLFgEYU
---
+++ src/games/calc.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import random from '../random.js';
+
+console.log('What is the result of the expression?');
+
+const calc = () => {
+  const firstOperand = random(1, 50);
+  const secondOperand = random(1, 50);
+  const operator = random(1, 3);
+  let answer;
+  switch (operator) {
+    case 1:
+      console.log(`Question: ${firstOperand} + ${secondOperand}`);
+      answer = String(firstOperand + secondOperand);
+      break;
+    case 2:
+      console.log(`Question: ${firstOperand} - ${secondOperand}`);
+      answer = String(firstOperand - secondOperand);
+      break;
+    case 3:
+      console.log(`Question: ${firstOperand} * ${secondOperand}`);
+      answer = String(firstOperand * secondOperand);
+      break;
+    default:
+      break;
+  }
+  const userAnswer = readlineSync.question('Your answer: ');
+  return [answer, userAnswer];
+};
+
+export default calc;
